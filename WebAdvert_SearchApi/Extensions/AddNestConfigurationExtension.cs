2ce4d1f8e0dca85c060c1bfcd0ad16e4e5e91e5a
Borislav Slavov <bobi_slavov@yahoo.com>
2021-03-15 16:42:57 +0200

    Add project files.

---
+++ WebAdvert_SearchApi/Extensions/AddNestConfigurationExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using WebAdvert_SearchApi.Models;
+
+namespace WebAdvert_SearchApi.Extensions
+{
+    public static class AddNestConfigurationExtension
+    {
+        public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            IConfigurationSection elasticSeacrhSection = configuration.GetSection("ES");
+            var elasticSearchUrl = elasticSeacrhSection.GetValue<string>("url");
+            string userName = elasticSeacrhSection.GetValue<string>("UserName");
+            string password = elasticSeacrhSection.GetValue<string>("Password");
+
+            var connectionSettings = new ConnectionSettings(new Uri(elasticSearchUrl))
+                .BasicAuthentication(userName, password)
+                .DefaultIndex("adverts")
+                .DefaultMappingFor<AdvertType>(d => d.IndexName("advert"))
+                .DefaultMappingFor<AdvertType>(advert => advert.IdProperty(p => p.Id));
+
+            var client = new ElasticClient(connectionSettings);
+
+            services.AddSingleton<IElasticClient>(client);
+        }
+    }
+}
---
+++ WebAdvert_SearchApi/Services/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAdvert_SearchApi.Models;
+
+namespace WebAdvert_SearchApi.Services
+{
+    public interface ISearchService
+    {
+        Task<List<AdvertType>> Search(string keyword);
+    }
+}
